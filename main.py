bb44f3c91536556fbaf150f13c9c4f0a38bddbab
Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>
2019-05-13 23:21:52 -0400

    .kv file updated. main py updated. some bug occured. there is no player 2 on the board

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ from kivy.clock import Clock
 from random import randint
 
 
+class Paddle(Widget):
+    score = NumericProperty()
+
+    def bounce(self, ball):
+        if self.collide_widget(ball):
+            velo_x, velo_y = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * velo_x, velo_y)
+            velo = bounced * 1.1
+            ball.velocity = velo.x, velo.y + offset
+
+
 class Ball(Widget):
-    x_axis_velocity = NumericProperty()
-    y_axis_velocity = NumericProperty()
+    x_axis_velocity = NumericProperty(0)
+    y_axis_velocity = NumericProperty(0)
 
     velocity = ReferenceListProperty(x_axis_velocity, y_axis_velocity)
 
@@ -18,19 +30,38 @@ class Ball(Widget):
 
 class Game(Widget):
     ball = ObjectProperty(None)
+    p1 = ObjectProperty(None)
+    p2 = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self, velo=(4,0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = velo
 
     def update(self, dt):
         self.ball.move_ball()
 
+        self.p1.bounce(self.ball)
+        self.p2.bounce(self.ball)
+
         if(self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.y_axix_velocity *= -1
 
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.x_axis_velocity *= -1
+        # if (self.ball.x < 0) or (self.ball.right > self.width):
+        #     self.ball.x_axis_velocity *= -1
+
+        if self.ball.x < self.x:
+            self.p2.score += 1
+            self.serve_ball(velo=(4, 0))
+
+        if self.ball.x > self.width:
+            self.p1.score += 1
+            self.serve_ball(velo=(4, 0))
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.p1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.p2.center_y = touch.y
 
 
 class GameApp(App):
